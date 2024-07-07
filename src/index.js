ec5289708ae1ad627398396fea9db7c93f3034d3
euan.may24@gmail.com <euan.may24@gmail.com>
2024-07-07 20:13:18 +0900

    손 아이콘 및 버튼 추가

--- src/index.js
+++ src/index.js
@@ -1,30 +1,4 @@
 import ReactDOM from 'react-dom';
+import App from './App';
 
-const WINS = {
-  rock: 'scissor',
-  scissor: 'paper',
-  paper: 'rock',
-};
-
-function getResult(left, right) {
-  if (WINS[left] === right) return '승리';
-  else if (left === WINS[right]) return '패배';
-  return '무승부';
-}
-
-function handleClick() {
-  console.log('가위바위보!');
-}
-
-const me = 'rock';
-const other = 'scissor';
-
-ReactDOM.render(
-  <>
-    <h1>가위바위보</h1>
-    <button>가위</button>
-    <button>바위</button>
-    <button>보</button>
-  </>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
